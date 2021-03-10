aaaf25f318a9eab2834377605c50131ef752904a
keika <jesicamorocho.m@gmail.com>
2021-03-10 16:07:08 +0100

    adding cors dependencies

--- source/app.js
+++ source/app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = require('./router');
+const cors = require('cors')
 const db = require('./db');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 app.use(router);
+
 
 
 
